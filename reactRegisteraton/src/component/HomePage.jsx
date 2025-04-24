fae57a0b01a59bfde53d35a87e325d86463affef
Asmitumrao <ashmitumrao@gmail.com>
2025-04-24 13:38:47 +0530

    error correction

---
+++ reactRegisteraton/src/component/HomePage.jsx
@@ -0,0 +1,27 @@
+// import React from 'react'
+// import { useNavigate } from 'react-router-dom'
+import { Link,Outlet } from 'react-router-dom'
+
+const HomePage = () => {
+  return (
+    <div>
+        Welcome to my Project
+        {/* {
+            setTimeout(() => {
+                navigate('/login');
+            }, 3000)
+        } */}
+
+        <nav>
+            <ul>
+                <li><Link to='/login'>Login</Link></li>
+                <li><Link to='/register'>Register</Link></li>
+            </ul>
+            
+        </nav>
+        <Outlet/>
+    </div>
+  )
+}
+
+export default HomePage

98aec4f364cdd9c6f4e91d98c01d17d903a0556e
Asmitumrao <ashmitumrao@gmail.com>
2025-04-24 12:45:06 +0530

    error resolved

--- reactRegisteraton/src/App.jsx
+++ reactRegisteraton/src/App.jsx
@@ -4,7 +4,6 @@ import Login from './component/Login'
 import Register from './component/Registeration'
 import Dashboard from './component/Dashboard'
 import { Route,Routes } from 'react-router-dom';
-import HomePage from './component/homePage'
 
 function App() {
  
